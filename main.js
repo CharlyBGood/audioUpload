e771859dadb2aa7e0d294e9e18cd27805c8ce47c
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2023-01-09 20:48:39 -0300

    create forms

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const file = document.getElementById("file");
-file.addEventListener("change", (e) => {
-  fileReader(file.files);
+const audioFile = document.getElementById("audio-file");
+audioFile.addEventListener("change", (e) => {
+  fileReader(audioFile.files);
 });
 
 let result = document.querySelector(".result");
+// let titleOne = `<h1 contenteditable="true">Modifica este título</h1>`;
+// let contentP = `<p contenteditable="true">Agrega una descripción aquí</p>`;
 
 const fileReader = (ar) => {
   for (let i = 0; i < ar.length; i++) {
@@ -13,11 +15,10 @@ const fileReader = (ar) => {
     reader.readAsDataURL(ar[i]);
     reader.addEventListener("load", (e) => {
       let newAudio = `<audio controls src='${e.currentTarget.result}'>`;
-      let titleOne = `<h1 contenteditable="true">Modifica este título</h1>`;
-      let contentP = `<p contenteditable="true">Agrega una descripción aquí</p>`;
+
       result.style.display = "flex";
-      result.innerHTML += titleOne;
-      result.innerHTML += contentP;
+      // result.innerHTML += titleOne;
+      // result.innerHTML += contentP;
       result.innerHTML += newAudio += "<hr>";
     });
   }
