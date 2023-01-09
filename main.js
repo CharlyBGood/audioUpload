555ca570daec3e74bd5cebcca17c8131dc031b97
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2023-01-09 19:56:46 -0300

    add section to add text and add some css

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ file.addEventListener("change", (e) => {
   fileReader(file.files);
 });
 
+let result = document.querySelector(".result");
+
 const fileReader = (ar) => {
   for (let i = 0; i < ar.length; i++) {
     const reader = new FileReader();
@@ -12,8 +14,11 @@ const fileReader = (ar) => {
     reader.addEventListener("load", (e) => {
       let newAudio = `<audio controls src='${e.currentTarget.result}'>`;
       let titleOne = `<h1 contenteditable="true">Modifica este título</h1>`;
-      document.querySelector(".result").innerHTML += titleOne;
-      document.querySelector(".result").innerHTML += newAudio += "<hr>";
+      let contentP = `<p contenteditable="true">Agrega una descripción aquí</p>`;
+      result.style.display = "flex";
+      result.innerHTML += titleOne;
+      result.innerHTML += contentP;
+      result.innerHTML += newAudio += "<hr>";
     });
   }
 };
