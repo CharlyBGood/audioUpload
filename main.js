201fcdba5e307923cb0d8e9d829bd3ce6cbe2be4
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2023-01-09 22:28:24 -0300

    add php file to upload files into server dir

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const fileReader = (ar) => {
     const reader = new FileReader();
     reader.readAsDataURL(ar[i]);
     reader.addEventListener("load", (e) => {
+      // let newAudio = `<audio controls controlsList="nodownload" src='${e.currentTarget.result}'>`;
       let newAudio = `<audio controls controlsList="nodownload" src='${e.currentTarget.result}'>`;
-
       result.style.display = "flex";
       // result.innerHTML += titleOne;
       // result.innerHTML += contentP;
@@ -23,3 +23,24 @@ const fileReader = (ar) => {
     });
   }
 };
+
+//  upload form
+
+document.getElementById("uploadForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let formData = new FormData();
+  formData.append("audio-file", document.getElementById("audio-file").files[0]);
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "upload.php", true);
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      alert("Archivo subido con éxito");
+    } else {
+      alert("Error al subir el archivo");
+    }
+  };
+
+  xhr.send(formData);
+});
---
+++ upload.php
@@ -0,0 +1,11 @@
+<?php
+  $targetDir = "audio/";
+  $fileName = basename($_FILES["audio-file"]["name"]);
+  $targetFilePath = $targetDir . $fileName;
+  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+  if (move_uploaded_file($_FILES["audio-file"]["tmp_name"], $targetFilePath)) {
+    echo "El archivo ". $fileName. " ha sido subido con éxito.";
+  } else {
+    echo "Ha ocurrido un error al subir el archivo.";
+  }
