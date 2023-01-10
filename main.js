99ad44e3180dbf1df72bd45c6cb3f456e65a9111
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2023-01-10 19:25:32 -0300

    edit form so just audio input shows

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ audioFile.addEventListener("change", (e) => {
 });
 
 let result = document.querySelector(".result");
-// let titleOne = `<h1 contenteditable="true">Modifica este título</h1>`;
-// let contentP = `<p contenteditable="true">Agrega una descripción aquí</p>`;
+let titleOne = `<h1 contenteditable="true">Modifica este título</h1>`;
+let contentP = `<p contenteditable="true">Agrega una descripción aquí</p>`;
 
 const fileReader = (ar) => {
   for (let i = 0; i < ar.length; i++) {
     const reader = new FileReader();
     reader.readAsDataURL(ar[i]);
     reader.addEventListener("load", (e) => {
-      // let newAudio = `<audio controls controlsList="nodownload" src='${e.currentTarget.result}'>`;
       let newAudio = `<audio controls controlsList="nodownload" src='${e.currentTarget.result}'>`;
       result.style.display = "flex";
-      // result.innerHTML += titleOne;
-      // result.innerHTML += contentP;
+      result.innerHTML += titleOne;
+      result.innerHTML += contentP;
       result.innerHTML += newAudio += "<hr>";
     });
   }
--- upload.php
+++ upload.php
@@ -1,11 +1,12 @@
 <?php
-  $targetDir = "audio/";
-  $fileName = basename($_FILES["audio-file"]["name"]);
-  $targetFilePath = $targetDir . $fileName;
-  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+$title = $_POST["title"];
+$targetDir = "audio/";
+$fileName = basename($_FILES["audio-file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
-  if (move_uploaded_file($_FILES["audio-file"]["tmp_name"], $targetFilePath)) {
-    echo "El archivo ". $fileName. " ha sido subido con éxito.";
-  } else {
-    echo "Ha ocurrido un error al subir el archivo.";
-  }
+if (move_uploaded_file($_FILES["audio-file"]["tmp_name"], $targetFilePath)) {
+  echo "El archivo " . $fileName . " ha sido subido con éxito.";
+} else {
+  echo "Ha ocurrido un error al subir el archivo.";
+}
